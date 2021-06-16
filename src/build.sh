02fbd096b4eb09984ad4d4152e67e618b8a781e9
Quentin Quadrat <lecrapouille@gmail.com>
2021-06-17 00:19:33 +0200

    Add build.sh

---
+++ src/build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# g++ -W -Wall --std=c++11 -g -O2 -DBACKWARD_HAS_DW=1 backward.cpp GUI.cpp Petri.cpp main.cpp -o PetriNetEditor `pkg-config --cflags --libs sfml-graphics` -ldw
+
+g++ -W -Wall --std=c++11 GUI.cpp Petri.cpp main.cpp -o PetriNetEditor `pkg-config --cflags --libs sfml-graphics`
