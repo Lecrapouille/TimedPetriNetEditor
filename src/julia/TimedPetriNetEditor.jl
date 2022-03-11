3dddfd4978b5a753982ef8ada46cd864e98ae1ce
Quentin Quadrat <lecrapouille@gmail.com>
2022-03-11 20:23:48 +0100

    Update date on headers

--- src/julia/TimedPetriNetEditor.jl
+++ src/julia/TimedPetriNetEditor.jl
@@ -1,6 +1,6 @@
 ##=====================================================================
 ## TimedPetriNetEditor: A timed Petri net editor.
-## Copyright 2021 Quentin Quadrat <lecrapouille@gmail.com>
+## Copyright 2021 -- 2022 Quentin Quadrat <lecrapouille@gmail.com>
 ##
 ## This file is part of TimedPetriNetEditor.
 ##
