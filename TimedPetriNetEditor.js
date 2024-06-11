f0af3a55a7547a8407d0c9a51d83832b4a313c71
GitHub CI <github-ci@users.noreply.github.com>
2024-06-11 21:29:39 +0000

    Automated commit

--- TimedPetriNetEditor.js
+++ TimedPetriNetEditor.js
@@ -39,7 +39,7 @@ if (ENVIRONMENT_IS_NODE) {
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: /tmp/tmp57k9lgce.js
+// include: /tmp/tmp9863_b7f.js
 
   if (!Module.expectedDataFileDownloads) {
     Module.expectedDataFileDownloads = 0;
@@ -227,21 +227,21 @@ Module['FS_createPath']("/home/runner/work/TimedPetriNetEditor/TimedPetriNetEdit
 
   })();
 
-// end include: /tmp/tmp57k9lgce.js
-// include: /tmp/tmp221klkh0.js
+// end include: /tmp/tmp9863_b7f.js
+// include: /tmp/tmpt1rb3p9k.js
 
     // All the pre-js content up to here must remain later on, we need to run
     // it.
     if (Module['$ww'] || (typeof ENVIRONMENT_IS_PTHREAD != 'undefined' && ENVIRONMENT_IS_PTHREAD)) Module['preRun'] = [];
     var necessaryPreJSTasks = Module['preRun'].slice();
-  // end include: /tmp/tmp221klkh0.js
-// include: /tmp/tmpt49u5zb_.js
+  // end include: /tmp/tmpt1rb3p9k.js
+// include: /tmp/tmp2njs95sn.js
 
     if (!Module['preRun']) throw 'Module.preRun should exist because file support used it; did a pre-js delete it?';
     necessaryPreJSTasks.forEach((task) => {
       if (Module['preRun'].indexOf(task) < 0) throw 'All preRun tasks that exist before user pre-js code should remain after; did you replace Module or modify Module.preRun?';
     });
-  // end include: /tmp/tmpt49u5zb_.js
+  // end include: /tmp/tmp2njs95sn.js
 
 
 // Sometimes an existing Module object exists with properties
